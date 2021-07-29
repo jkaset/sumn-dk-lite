3141fa4235d7084360f1d0a41b6ef4164429b5e7
Leslie Crosby <lesliercrosby@gmail.com>
2021-07-29 15:24:11 -0500

    modify scripts, esbuild config for this project

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import App from './app'
 
-const LOCAL_API_URL = 'http://localhost:1234'
+const LOCAL_JS_URL = 'http://localhost:1234/index.js'
 const isStaging = window.location.host.match(/webflow.io/)
 
 if (isStaging && !window.__DK__) {
-  $.getScript(`${LOCAL_API_URL}/index.js`)
+  $.getScript(LOCAL_JS_URL)
     .done(() => {
       process.env.NODE_ENV === 'production' || console.log('🙊 DK Lite Started')
       window.__DK__ = true
